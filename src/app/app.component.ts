1ea82d35fdfdf62f8857eab1913b612cd96a8500
jpupava <jpupava@student.umb.sk>
2022-03-02 21:45:31 +0100

    Nefunguje formGroup, ale už viem spustiť ten projekt. :D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,50 +6,59 @@ import {FormControl, FormGroup} from "@angular/forms";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Convert DEC/BIN';
+
+export class AppComponent{
+  title = 'Konvertor medzi DEC a BIN ';
   convertor: FormGroup;
 
-  constructor() {
+  constructor(){
     this.convertor = new FormGroup({
-      dec: new FormControl(),
-      b7: new FormControl(),
-      b6: new FormControl(),
-      b5: new FormControl(),
-      b4: new FormControl(),
-      b3: new FormControl(),
-      b2: new FormControl(),
-      b1: new FormControl(),
-      b0: new FormControl(),
+
+      dec0: new FormControl(),
+
+      bin7: new FormControl(),
+      bin6: new FormControl(),
+      bin5: new FormControl(),
+      bin4: new FormControl(),
+
+      bin3: new FormControl(),
+      bin2: new FormControl(),
+      bin1: new FormControl(),
+      bin0: new FormControl(),
     })
-    this.convertor.controls["dec"].setValue(0);
+
+    this.convertor.controls["dec0"].setValue(0);
+  }
+
+  private uncheckAll(){
+    for (let i = 0; i < 8; i++){
+      this.convertor.controls["bin" + i].setValue("");
+    }
   }
 
   public toDec() {
     let value = 0;
     let current = 1;
     for (let i = 0; i < 8; i++) {
-      if (this.convertor.controls["b" + i].value) { value += current; }
+      if (this.convertor.controls["bin" + i].value){
+        value += current;
+      }
       current *= 2;
     }
-    this.convertor.controls["dec"].setValue(value);
+    this.convertor.controls["dec0"].setValue(value);
   }
 
   public toBin() {
-    let value = this.convertor.controls["dec"].value;
+    let value = this.convertor.controls["dec0"].value;
     let current = 128;
     this.uncheckAll()
-    for (let i = 7; i >= 0; i--) {
-      if (value >= current) {
+    for (let i = 7; i >= 0; i--){
+      if (value >= current){
         value -= current;
-        this.convertor.controls["b" + i].setValue("checked");
+        this.convertor.controls["bin" + i].setValue("checked");
       }
       if (value == 0) { break; }
       current /= 2;
     }
-  }
-
-  private uncheckAll() {
-    for (let i = 0; i < 8; i++) { this.convertor.controls["b" + i].setValue(""); }
   }
 }
