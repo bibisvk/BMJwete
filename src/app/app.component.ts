f0ec8e390da48e1814debe9fb4540de7a18c9279
Bianka Modrovičová <bibisvk@gmail.com>
2022-03-02 22:40:47 +0100

    add osoby form and table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,30 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+
+
+enum MENU {OSOBY,KNIHY,VYPOZICKY}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+export class AppComponent {
+  zobraz = false;
 
-export class AppComponent{
-  title = 'Konvertor medzi DEC a BIN ';
-  convertor: FormGroup;
+  menu = MENU;
+  aktMenu = MENU.OSOBY;
 
-  constructor(){
-    this.convertor = new FormGroup({
+  osoby: any = [];
+  osoba = {o_id: "0", meno: "aaa", priezvisko: "bbb"};
 
-      dec0: new FormControl(),
-
-      bin7: new FormControl(),
-      bin6: new FormControl(),
-      bin5: new FormControl(),
-      bin4: new FormControl(),
-
-      bin3: new FormControl(),
-      bin2: new FormControl(),
-      bin1: new FormControl(),
-      bin0: new FormControl(),
-    })
-
-    this.convertor.controls["dec0"].setValue(0);
+  public pridajOsobu(){
+    let o2 = {o_id: this.osoba.o_id, meno: this.osoba.meno, priezvisko: this.osoba.priezvisko};
+    this.osoby.push(o2);
+    this.zobraz = true;
   }
 
-  private uncheckAll(){
-    for (let i = 0; i < 8; i++){
-      this.convertor.controls["bin" + i].setValue("");
-    }
+  public nastavMenu(m: MENU){
+    this.aktMenu = m;
   }
 
-  public toDec() {
-    let value = 0;
-    let current = 1;
-    for (let i = 0; i < 8; i++) {
-      if (this.convertor.controls["bin" + i].value){
-        value += current;
-      }
-      current *= 2;
-    }
-    this.convertor.controls["dec0"].setValue(value);
-  }
-
-  public toBin() {
-    let value = this.convertor.controls["dec0"].value;
-    let current = 128;
-    this.uncheckAll()
-    for (let i = 7; i >= 0; i--){
-      if (value >= current){
-        value -= current;
-        this.convertor.controls["bin" + i].setValue("checked");
-      }
-      if (value == 0) { break; }
-      current /= 2;
-    }
-  }
 }
