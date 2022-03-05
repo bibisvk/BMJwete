065cdad1e9ec7945b5fe126106f240f2f86aa526
mfulajtar <mfulajtar@umb.sk>
2022-03-05 13:46:47 +0100

    module kniha upravy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { OsobaFormularComponent } from './osoba-formular/osoba-formular.component';
 import { OsobaStrankaComponent } from './osoba-stranka/osoba-stranka.component';
 import { OsobyZoznamComponent } from './osoby-zoznam/osoby-zoznam.component';
+import { KnihaFormularComponent } from './kniha-formular/kniha-formular.component';
+import { KnihaStrankaComponent } from './kniha-stranka/kniha-stranka.component';
+import { KnihyZoznamComponent } from './knihy-zoznam/knihy-zoznam.component';
 import {RouterModule} from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 
@@ -14,7 +17,10 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     OsobaFormularComponent,
     OsobaStrankaComponent,
-    OsobyZoznamComponent
+    OsobyZoznamComponent,
+    KnihaFormularComponent,
+    KnihaStrankaComponent,
+    KnihyZoznamComponent,
   ],
     imports: [
         BrowserModule,
