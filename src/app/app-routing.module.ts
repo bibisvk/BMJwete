9d070fcf1480c60268d4e4df5e56f8903ca0f060
mfulajtar <mfulajtar@umb.sk>
2022-03-05 14:52:17 +0100

    module kniha upravy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import {RouterModule, Routes} from "@angular/router";
 import {OsobaStrankaComponent} from "./osoba-stranka/osoba-stranka.component";
+import {KnihaStrankaComponent} from "./kniha-stranka/kniha-stranka.component";
 import {NgModule} from "@angular/core";
 
-const routes: Routes = [
+const routesosoba: Routes = [
   {
     path: 'osoba',
-    component: OsobaStrankaComponent
+    component: OsobaStrankaComponent,
+  }
+]
+const routeskniha: Routes = [
+  {
+    path: 'kniha',
+    component: KnihaStrankaComponent,
   }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routesosoba)],
+  exports: [RouterModule]
+})
+@NgModule({
+  imports: [RouterModule.forRoot(routeskniha)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
