04f0786b82c748278e4d1cce6fbb772590793fb4
mfulajtar <mfulajtar@umb.sk>
2022-03-05 16:13:32 +0100

    upravenie module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import {RouterModule, Routes} from "@angular/router";
 import {OsobaStrankaComponent} from "./osoba-stranka/osoba-stranka.component";
+import {KnihaStrankaComponent} from "./kniha-stranka/kniha-stranka.component";
 import {NgModule} from "@angular/core";
 
 const routes: Routes = [
@@ -8,9 +9,20 @@ const routes: Routes = [
     component: OsobaStrankaComponent
   }
 ]
+const routeskniha: Routes = [
+  {
+    path: 'kniha',
+    component: KnihaStrankaComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+@NgModule({
+  imports: [RouterModule.forRoot(routeskniha)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
