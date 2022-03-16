32f8506db533f0a82e74e2f9d005a1e53dc9e6a4
mfulajtar <mfulajtar@umb.sk>
2022-03-16 16:57:08 +0100

    kniha uprav a zmaz

--- src/app/kniha-stranka/kniha-stranka.component.ts
+++ src/app/kniha-stranka/kniha-stranka.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import {Router} from "@angular/router";
 import {Kniha} from "../models/kniha.model";
+import {Osoba} from "../models/osoba.model";
 
 @Component({
   selector: 'app-kniha-stranka',
@@ -20,6 +21,23 @@ export class KnihaStrankaComponent{
 
   pridaj(kniha: Kniha): void {
     this.knihy.push(kniha);
+  }
+  uprav(kniha: Kniha): void {
+    const index = this.knihy.findIndex(knihaArray => knihaArray.k_id === kniha.k_id);
+    if (index !== -1) {
+      this.knihy[index] = kniha;
+    }
+  }
+
+  upravZoZoznamu(kniha: Kniha): void {
+    this.knihaNaUpravu = kniha;
+  }
+
+  zmazZoZoznamu(kniha: Kniha): void {
+    const index = this.knihy.findIndex(knihaArray => knihaArray.k_id === kniha.k_id);
+    if (index !== -1) {
+      this.knihy.splice(index, 1);
+    }
   }
 
 }
